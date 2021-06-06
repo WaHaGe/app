cecb0fd0a65a5787fcd1bdfee250e12d83e0e3b4
Wakiza Halian Genesee <whgfourth@outlook.com>
2021-06-05 20:36:57 -0400

    index

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/main', methods=['GET', 'POST'])
 def hello_world():
-    return True
+    return render_template('index.md')
 
 if __name__ == '__main__':
     app.run()
